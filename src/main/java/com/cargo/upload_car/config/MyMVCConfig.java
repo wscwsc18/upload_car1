9d7f13dbb86b107d918ac90d38708c11f5824817
baron57 <18841255877@163.com>
2019-10-30 15:19:36 +0800

    10.30 下午3.20 默认访问路径修改

---
+++ src/main/java/com/cargo/upload_car/config/MyMVCConfig.java
@@ -0,0 +1,22 @@
+package com.cargo.upload_car.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+public class MyMVCConfig extends WebMvcConfigurerAdapter {
+    // 所有的WebMvcConfigurerAdapter组件都会一起起作用
+    @Bean // 将组件注册在容器
+    public WebMvcConfigurerAdapter webMvcConfigurerAdapter() {
+        WebMvcConfigurerAdapter adapter = new WebMvcConfigurerAdapter() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/").setViewName("index");
+                registry.addViewController("/index.html").setViewName("index");
+            }
+
+        };
+        return adapter;
+    }
+
+}
---
+++ src/main/java/com/cargo/upload_car/controller/FerstController.java
@@ -0,0 +1,12 @@
+package com.cargo.upload_car.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class FerstController {
+
+//    @RequestMapping({"/","/index.html"})
+//    public String showIndex(){
+//        return "index";
+//    }
+}
